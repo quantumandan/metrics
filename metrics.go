0f7ff8dfd2051b9c39dc2e28734a431619687334
Dan Cohn <dancohn@Dans-MacBook-Air.local>
2021-11-11 13:40:24 -0500

    little bit of cleanup and some commentary

--- metrics.go
+++ metrics.go
@@ -8,13 +8,23 @@ import (
 	"github.com/quantumandan/metrics/hamming"
 )
 
+// Main function for dan's edit distance adventure in golang
 func main() {
-    fmt.Println(levenshtein.EditDistance("intention", "execution"))
-    fmt.Println(hamming.EditDistance("intention", "execution"))
-    fmt.Println(levenshtein.EditDistance("horse", "ros"))
-    fmt.Println(hamming.EditDistance("horse", "ros"))
-    fmt.Println(levenshtein.EditDistance("karolin", "kathrin"))
-    fmt.Println(hamming.EditDistance("karolin", "kathrin"))
-    fmt.Println(levenshtein.EditDistance("kitten", "sitting"))
-    fmt.Println(hamming.EditDistance("kitten", "sitting"))
+    fmt.Println("Compare and contrast Levenshtein vs Hamming metrics")
+
+    fmt.Println(levenshtein.EditDistance("intention", "execution"))  // 5
+    fmt.Println(hamming.EditDistance("intention", "execution"))      // 5
+
+    fmt.Println(levenshtein.EditDistance("horse", "ros"))            // 3
+    fmt.Println(hamming.EditDistance("horse", "ros"))                // 0
+    
+    fmt.Println(levenshtein.EditDistance("karolin", "kathrin"))      // 3
+    fmt.Println(hamming.EditDistance("karolin", "kathrin"))          // 3
+
+    fmt.Println(levenshtein.EditDistance("kitten", "sitting"))       // 3
+    fmt.Println(hamming.EditDistance("kitten", "sitting"))           // 0
+
+    // would be really interesting to examing the algebraic and topological
+    // connections of the Levenshtein edit distance matrix D and compare it
+    // to the equivalent in Hamming for n-length strings.
 }
